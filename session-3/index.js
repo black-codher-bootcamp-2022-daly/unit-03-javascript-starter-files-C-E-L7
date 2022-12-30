f5b4492b9c285ef07e5d2435adc38d13348260e2
C-E-L7 <carrera.eley@gmail.com>
2022-12-30 17:37:55 +0000

    sorted tasks 2, added task  3

--- session-3/index.js
+++ session-3/index.js
@@ -1,4 +1,5 @@
 // This is a comment in JavaScript
+// This is a comment in JavaScript Task 2 of session 3 below
 const userName = "Carrera";
 
 function sayHey() {
@@ -11,4 +12,14 @@ function conversation() {
   console.log("Goodbye");
 }
 conversation();
+// You can see where this file is connected to your HTML in the index.html on line 14
+
+// This is a comment in JavaScript Task 3 of session 3 below
+
+function futureAge(name, age) {
+  const future = age + 5;
+  return "Hi " + name + " You will be " + future + " in 5 years.";
+}
+
+console.log(futureAge("Carrera", 36));
 // You can see where this file is connected to your HTML in the index.html on line 14
