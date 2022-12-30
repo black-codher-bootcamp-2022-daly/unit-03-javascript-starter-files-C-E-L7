81a3e9500a9f62ede99cd70c6c238039c2ac1b26
C-E-L7 <carrera.eley@gmail.com>
2022-12-30 10:33:40 +0000

    Changed code to functions

--- session-3/index.js
+++ session-3/index.js
@@ -1,15 +1,14 @@
 // This is a comment in JavaScript
+const userName = "Carrera";
 
-let a = 5;
-let b = 10;
-let c = a + b;
-//line left blank for readability
-console.log(c); //Expected output = 15
+function sayHey() {
+  console.log("Hey" + userName);
+}
 
-a = 20;
-
-console.log(c); //Expected output = 30 but its still 15. The next codes will make c to = 30
-c = a + b;
-console.log(c);
-//Now the output value of C is 30
+function conversation() {
+  sayHey();
+  console.log("How are you today?");
+  console.log("Goodbye");
+}
+conversation();
 // You can see where this file is connected to your HTML in the index.html on line 14
