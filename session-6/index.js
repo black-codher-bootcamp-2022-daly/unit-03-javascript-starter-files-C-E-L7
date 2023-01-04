a6b7b7f99565d7a93223b597bf108cace3dfd28f
C-E-L7 <carrera.eley@gmail.com>
2023-01-04 16:03:04 +0000

    added task that needs completing

--- session-6/index.js
+++ session-6/index.js
@@ -1,31 +1,24 @@
+// ****************************************************TASK 1****************************************************
+
 // Use this randomNumber inside of the while/for loops to generate a random number
 const randomNumber = Math.round(Math.random() * 10);
 
-// ****************************************************TASK 1****************************************************
+let counter = 0;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+while (counter < 10) {
+  const randomNumber = Math.round(Math.random() * 10);
+  console.log(`random number`, randomNumber);
+  counter++;
+}
+//while(counter < 10) {
+//const randomNumber = Math.round(Math.random() * 10);
+//console.log(`random number`,randomNumber)
+//counter++
+//}
 
 // ****************************************************TASK 2****************************************************
+let randomNumber = Math.round(Math.random() * 10);
 
-
-
-
-
-
-
-
-
-
-
+for (i = 1; i < 10; i++) {
+  console.log(`random number`, randomNumber);
+}
