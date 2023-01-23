c40f0f537232b5e9b026616c0d7cfe301307a477
C-E-L7 <carrera.eley@gmail.com>
2023-01-23 15:53:11 +0000

    simplified code

--- session-6/index.js
+++ session-6/index.js
@@ -1,13 +1,12 @@
 // ****************************************************TASK 1****************************************************
 
 // Use this randomNumber inside of the while/for loops to generate a random number
-const randomNumber = Math.round(Math.random() * 10);
+const randomNumber = Math.random() * 10;
 
 let counter = 0;
 
 while (counter < 10) {
-  const randomNumber = Math.round(Math.random() * 10);
-  console.log(`random number`, randomNumber);
+  console.log(randomNumber);
   counter++;
 }
 //while(counter < 10) {
@@ -17,6 +16,7 @@ while (counter < 10) {
 //}
 
 // ****************************************************TASK 2****************************************************
+
 let randomNumber = Math.round(Math.random() * 10);
 
 for (i = 1; i < 10; i++) {
